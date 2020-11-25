e1efd53eb7f51f76d08193b92e0059cfcef10430
Torofield <tanino@a2.mbn.or.jp>
2020-11-25 23:57:35 +0900

    fix: asdict for update returned

--- layer/infra/db/action.py
+++ layer/infra/db/action.py
@@ -4,6 +4,10 @@ from . import models
 from . import schemas
 
 
+import logging
+logger = logging.getLogger(__name__)
+
+
 def get_addrs(db: Session):
     return db.query(models.Addr).all()
 
@@ -23,6 +27,19 @@ def create_addr_query(db: Session, addr: schemas.AddrCreateEntity):
     return db_addr
 
 
+from collections import OrderedDict
+
+
+def asdict(self):
+    result = OrderedDict()
+    for key in self.__mapper__.c.keys():
+        if getattr(self, key) is not None:
+            result[key] = str(getattr(self, key))
+        else:
+            result[key] = getattr(self, key)
+    return result
+
+
 def update_addr_query(db: Session, addr: schemas.AddrUpdateEntity):
     db_addr = db.query(models.Addr).filter(models.Addr.id==addr.id).first()
 
@@ -32,6 +49,8 @@ def update_addr_query(db: Session, addr: schemas.AddrUpdateEntity):
     db_addr.update_time = datetime.datetime.now()
 
     db.commit()
+    # 辞書に変更しないと、modelsがそのまま反映されてしまうため
+    asdict(db_addr)
     return db_addr
 
 
